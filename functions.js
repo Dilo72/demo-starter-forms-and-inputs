ae0e5f90113554beddfd2ba3df179cdbe3faabd2
Diana Isela <80071683+Dilo72@users.noreply.github.com>
2021-05-21 17:46:11 -0400

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,16 +1,12 @@
+// use string argument to create and append <p> tag to the story output article element 
+let storyDisplay = document.querySelector(`#storyDisplay`)
+console.log(storyDisplay)
 
-// use a string argument to create and append a <p> tag to the story display <article> element 
-
-let storyDisplay = document.querySelector('#storyDisplay')
-
-// console.log(storyDisplay)
-
-let appendParagraph = function ( stringToAppendToArticle ) {
-    let paragraph = document.createElement('p')
-    paragraph.append(stringToAppendToArticle)
-    storyDisplay.append(paragraph)
+let appendParagraph = function (stringtoAppendToArticle) {
+let paragraph = document.createElement(`p`)
+paragraph.append(stringtoAppendToArticle)
+storyDisplay.append(paragraph)
 }
 
 appendParagraph(pages[0])
 
-// line order = 4, 6, 8, 14, 9, 10, 11
