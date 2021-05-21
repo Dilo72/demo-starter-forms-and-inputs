3e2aa90db671367d40f0ccc0420f24df4b90d6eb
Diana Isela <80071683+Dilo72@users.noreply.github.com>
2021-05-21 17:47:17 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,38 +1,32 @@
 
-// when the form is submitted we will be logging the event and modifying the page
-
 let nameForm = document.querySelector('#nameForm')
 console.log(nameForm)
 
-// addEventListener signature = ('eventName', callbackFunction )
-// callback signature = ( eventObject )
-nameForm.addEventListener('submit', function ( eventObject ) {
-    // watch out for this!!!
-    // default behavior is to refresh page on submit
-    eventObject.preventDefault()
-    // console.dir explicitly tells javascript to list all properties on the object
-    console.dir(eventObject)
+nameForm.addEventListener('submit', function (eventObject) {
+    
+ eventObject.preventDefault()
+console.dir(eventObject)
 
-    let nameInput = document.querySelector('#nameInput')
-    // get the value inside the input
-    let name = nameInput.value
-    appendParagraph('Welcome ' + name)
-    // `Welcome ${name}`
+
+let nameInput = document.querySelector('#nameInput')
+let name = nameInput.value
+appendParagraph('Welcome' + name)
 
 })
-
-// dont call your callback too early!
-// nameForm.addEventListener('submit', callback())
 
 let pageForm = document.querySelector('#pageForm')
 console.log(pageForm)
 
-pageForm.addEventListener('submit', function ( eventObject ) {
+pageForm.addEventListener(`submit`, function(eventObject){
     eventObject.preventDefault()
-    let pageInput = document.querySelector('input[type=number]')
+
+    let pageInput = document.querySelector(`input[type=number]`)
     console.log(pageInput)
+
     let pageNumber = pageInput.value
-    // check your indexing - is page one index 1?
     let page = pages[pageNumber]
-    appendParagraph(page)
+    appendParagraph(pages[pageNumber])
+
+
+
 })
